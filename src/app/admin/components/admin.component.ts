6dd0b2c7c549ce5231dadefe44de307f0564f86f
Ester Borges <esterborges95@gmail.com>
2025-02-15 15:45:00 -0300

    feat: fix css login module

--- src/app/admin/components/admin.component.ts
+++ src/app/admin/components/admin.component.ts
@@ -2,9 +2,12 @@ import { Component } from "@angular/core";
 
 @Component({
     template: `
-        <h2>Controle de ponto - Admin</h2>
-        <router-outlet></router-outlet>
+        <div class="content-admin">
+            <h2>Controle de ponto - Admin</h2>
+            <router-outlet></router-outlet>
+        </div>
     `,
+    styleUrls: ['./admin.component.scss'],
     host: { 'hostID': crypto.randomUUID().toString() }
     
 })
